7b02e30898dd5b29d09e6ba746c73721ddcbc0ac
symmagency <agencysymm@gmail.com>
2025-05-28 11:24:25 -0300

    Ajustes no cabeçalho

--- functions.js
+++ functions.js
@@ -1,17 +1,22 @@
 $(document).ready(function() {
 
+// alterações gerais
+$('.conteudo-topo .inferior .busca-mobile input').attr('placeholder','Buscar...');
+
 $(window).on('resize', function() {
 
     if ($(window).width() > 768) {
         //desktop
         $('.conteudo-topo .inferior .busca-mobile').after(`
         <div class="sign-up-header">
-            <a href="./conta/login?next=/conta/index">
+            <a href="../conta/login?next=/conta/index">
                 <span>Entrar</span>
                 <i class="user"></i>
             </a>
         </div>    
         `);
+        
+        $('#cabecalho .conteudo-topo').prepend($('#cabecalho .menu.superior'));
 
     } else {
         //mobile 
