adeb2ae5f634c45429b5c1271c74429dfe1714d7
symmagency <agencysymm@gmail.com>
2025-05-28 16:30:57 -0300

    Ajuste no entrar

--- functions.js
+++ functions.js
@@ -23,15 +23,18 @@ $('.banner.cheio li').each(function () {
 $(window).on('resize', function() {
 
     if ($(window).width() > 768) {
-        //desktop
-        $('.conteudo-topo .inferior .busca-mobile').after(`
-        <div class="sign-up-header">
-            <a href="../conta/login?next=/conta/index">
-                <span>Entrar</span>
-                <i class="user"></i>
-            </a>
-        </div>    
-        `);
+        // desktop
+        // Antes de adicionar, verifica se já existe para evitar duplicação
+        if ($('.conteudo-topo .sign-up-header').length === 0) {
+            $('.conteudo-topo .inferior .busca-mobile').after(`
+                <div class="sign-up-header">
+                    <a href="../conta/login?next=/conta/index">
+                        <span>Entrar</span>
+                        <i class="user"></i>
+                    </a>
+                </div>    
+            `);
+        }
 
         $('#cabecalho .conteudo-topo').prepend($('#cabecalho .menu.superior'));
 
