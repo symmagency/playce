76976c32d436e202d76b12185d098d5f88e4eeb0
symmagency <agencysymm@gmail.com>
2025-05-28 10:59:29 -0300

    Adição do "Entrar"

--- functions.js
+++ functions.js
@@ -1,5 +1,22 @@
 $(document).ready(function() {
 
-    
+$(window).on('resize', function() {
+
+    if ($(window).width() > 768) {
+        //desktop
+        $('.conteudo-topo .inferior .busca-mobile').after(`
+        <div class="sign-up-header">
+            <a href="./conta/login?next=/conta/index">
+                <span>Entrar</span>
+                <i class="user"></i>
+            </a>
+        </div>    
+        `);
+
+    } else {
+        //mobile 
+
+    }
+});
 
 });
