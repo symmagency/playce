fb7ed1cbc377e25489985b9fcaaf9c74812a7760
symmagency <agencysymm@gmail.com>
2025-06-27 23:14:40 -0300

    Ajustes no rodapé

--- functions.js
+++ functions.js
@@ -2,6 +2,7 @@ $(document).ready(function() {
 
 // alterações gerais
 $('.selos li:first-child img').attr('src','https://cdn.awsli.com.br/2830/2830294/arquivos/site-protegido.svg');
+$('#cabecalho .conteiner .row-fluid .logo a img').clone().prependTo($('#rodape>div:last-child .conteiner .row-fluid > .span9.span12'));
 $('.conteudo-topo .inferior .busca-mobile input').attr('placeholder','Buscar...');
 $('.pagina-login .cadastro-logar .form-horizontal .controls #id_email').attr('placeholder','E-mail');
 $('.pagina-login .cadastro-logar .form-horizontal .controls #id_senha').attr('placeholder','Senha');
